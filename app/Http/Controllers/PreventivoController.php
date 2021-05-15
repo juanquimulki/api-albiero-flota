ac34fb874abba4dc1d96659ccc6be15803e4b00d
Juan M. Mulki A <jmulki@sooft.com.ar>
2021-05-15 14:49:49 -0300

    Carga del período del evento

--- app/Http/Controllers/PreventivoController.php
+++ app/Http/Controllers/PreventivoController.php
@@ -19,6 +19,8 @@ class PreventivoController extends Controller
         $preventivo->detalles = $request->detalles;
         $preventivo->frecuenciaDias = $request->frecuenciaDias;
         $preventivo->frecuenciaKms = $request->frecuenciaKms;
+        $preventivo->ultimaFecha = $request->ultimaFecha;
+        $preventivo->ultimoKms = $request->ultimoKms;
 
         $save = $preventivo->save();
 
@@ -45,6 +47,8 @@ class PreventivoController extends Controller
         $preventivo->detalles = $request->detalles;
         $preventivo->frecuenciaDias = $request->frecuenciaDias;
         $preventivo->frecuenciaKms = $request->frecuenciaKms;
+        $preventivo->ultimaFecha = $request->ultimaFecha;
+        $preventivo->ultimoKms = $request->ultimoKms;
 
         $save = $preventivo->save();
 
